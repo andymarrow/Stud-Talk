9c5421d629aecb65ec987ee2fe12bb2decfffd47
miheretab <miheretabsamson90@gmail.com>
2023-09-07 06:29:31 -0700

    change next config

--- next.config.js
+++ next.config.js
@@ -4,11 +4,11 @@ const nextConfig = {
     ignoreBuildErrors:true,
   }, 
   
-  
   reactStrictMode: true,
   images:{
     domains:['images-workbench.99static.com','lh3.googleusercontent.com'],
   }
+  
 }
 
 module.exports = nextConfig
