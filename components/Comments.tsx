4bb310403912d51c99c982491b345501f60e6316
miheretab <miheretabsamson90@gmail.com>
2023-09-07 04:21:11 -0700

    Update code with new features

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -59,7 +59,7 @@ const Comments = ({ comment, setComment, addComment, comments, isPostingComment 
                         </div>
                       </Link>
                       <div>
-                        <p className='-mt-5 ml-16 text-[16px] mr-8 break-all'>
+                        <p className='-mt-5 ml-16 text-[16px] mr-8 whitespace-normal'>
                           {item.comment}
                         </p>
                       </div>
